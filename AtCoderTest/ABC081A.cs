81a4e60cad0751dc923dd39ff70c2ec9f427cde8
MasahiroTakahashi <take1931032@gmail.com>
2018-04-04 21:11:55 +0900

    問2解答

---
+++ AtCoderTest/ABC081A.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AtCoderBeginnersSelection
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            Console.WriteLine(num.Length - num.Replace("1","").Length );
+        }
+    }
+}
--- AtCoderTest/ABC086A.cs
+++ AtCoderTest/ABC086A.cs
@@ -1,20 +1,20 @@
-﻿using System;
-namespace AtCoderBeginnersSelection
-{
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            int A, B;
-            string[] num = Console.ReadLine().Split(' ');
-            A = int.Parse(num[0]);
-            B = int.Parse(num[1]);
-            if((A*B)%2 ==1){
-                Console.WriteLine("Odd");
-            }else{
-                Console.WriteLine("Even");
-            }
-        }
-    }
+﻿//using System;
+//namespace AtCoderBeginnersSelection
+//{
+//    class MainClass
+//    {
+//        public static void Main(string[] args)
+//        {
+//            int A, B;
+//            string[] num = Console.ReadLine().Split(' ');
+//            A = int.Parse(num[0]);
+//            B = int.Parse(num[1]);
+//            if((A*B)%2 ==1){
+//                Console.WriteLine("Odd");
+//            }else{
+//                Console.WriteLine("Even");
+//            }
+//        }
+//    }
 
-}
+//}
