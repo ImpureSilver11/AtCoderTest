b5177d315486c27d23e9e44e297e74e1a6e7b6eb
MasahiroTakahashi <take1931032@gmail.com>
2018-04-04 20:44:01 +0900

    　
    0点だったので修正

---
+++ AtCoderTest/AtCoderTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AtCoderTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int A, B, C;
+            string S;
+            A = int.Parse(Console.ReadLine());
+            string[] bandc = Console.ReadLine().Split(' ');
+            B = int.Parse(bandc[0]);
+            C = int.Parse(bandc[1]);
+            S = Console.ReadLine();
+            Console.WriteLine ((A+B+C) + " " +S);
+        }
+    }
+}
