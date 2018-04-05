a6b7759ec57edd339be344c144e6a8755b93c621
MasahiroTakahashi <take1931032@gmail.com>
2018-04-05 21:07:02 +0900

    問3解答中

--- AtCoderTest/ABC081A.cs
+++ AtCoderTest/ABC081A.cs
@@ -3,10 +3,10 @@ namespace AtCoderBeginnersSelection
 {
     class MainClass
     {
-        public static void Main(string[] args)
-        {
-            string num = Console.ReadLine();
-            Console.WriteLine(num.Length - num.Replace("1","").Length );
-        }
+        //public static void Main(string[] args)
+        //{
+        //    string num = Console.ReadLine();
+        //    Console.WriteLine(num.Length - num.Replace("1","").Length );
+        //}
     }
 }
---
+++ AtCoderTest/ABC081B.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+namespace AtCoderTest
+{
+    public class ABC081B
+    {
+        public static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            long[] num = Array.ConvertAll(
+                Console.ReadLine().Split(' '),
+                new Converter<string, long>(s => long.Parse(s) ));
+            int count = 0;
+            count = keisan(num ,count);
+            Console.WriteLine(count);
+        }
+        public static int keisan(long[] num,int count){
+            GC.Collect();
+            if(num.Sum() %2 ==1){
+                return count;
+            }else{
+                count++;
+                num = Array.ConvertAll<long,long>( num,waru);
+                return keisan(num, count);
+            }
+        }
+        public  static long  waru(long i){
+            return i / 2;
+        }
+    }
+}
