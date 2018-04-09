367aad2b5e2e1f1501c4973100874af107e3f6c8
MasahiroTakahashi <take1931032@gmail.com>
2018-04-09 20:33:16 +0900

    Some Sumsまで終了

---
+++ AtCoderTest/ABC083B.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace AtCoderTest
+{
+    class ABC083B{
+        //int count = 0;
+        //public ABC083B()
+        public static void Main(string[] args)
+        {
+            int count = 0;
+
+            string[] num = Console.ReadLine().Split(' ');
+            int a = int.Parse(num[1]);
+            int b = int.Parse(num[2]);
+            for (int i = 1; i <= int.Parse(num[0]);i++){
+                int sum = 0;
+                for (int j = 0; j <= i.ToString().Length-1;j++){
+                    sum += int.Parse(i.ToString().Substring(j,1));
+                }
+                if (a <= sum && sum <= b)
+                {
+                    count += i;
+                }
+            }
+            Console.WriteLine(count);
+        }
+
+		//public int getAns(){
+        //    return this.count;
+        //}
+    }
+}
--- AtCoderTest/main.cs
+++ AtCoderTest/main.cs
@@ -1,12 +1,12 @@
-﻿using System;
-namespace AtCoderTest
-{
-    public class main
-    {
-        public static void Main(string[] args)
-        {
-            ABC087B a = new ABC087B();
-            Console.WriteLine(a.getAns());
-        }
-    }
-}
+﻿//using System;
+//namespace AtCoderTest
+//{
+//    public class main
+//    {
+//        public static void Main(string[] args)
+//        {
+//            ABC083B a = new ABC083B();
+//            Console.WriteLine(a.getAns());
+//        }
+//    }
+//}
